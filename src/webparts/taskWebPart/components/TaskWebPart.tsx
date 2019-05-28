f983de80ed4df83149ff8781840e044f8eadafb8
Chad Kruszynsky <ckruszynsky@turner-industries.com>
2019-05-28 13:06:40 -0500

    added FluentCustomizer to web part

--- src/webparts/taskWebPart/components/TaskWebPart.tsx
+++ src/webparts/taskWebPart/components/TaskWebPart.tsx
@@ -1,11 +1,15 @@
-import * as React from 'react';
-import styles from './TaskWebPart.module.scss';
-import { ITaskWebPartProps } from './ITaskWebPartProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { FluentCustomizations } from '@uifabric/fluent-theme';
+import { Customizer } from 'office-ui-fabric-react';
+import * as React from 'react';
+
+import { ITaskWebPartProps } from './ITaskWebPartProps';
+import styles from './TaskWebPart.module.scss';
 
 export default class TaskWebPart extends React.Component<ITaskWebPartProps, {}> {
   public render(): React.ReactElement<ITaskWebPartProps> {
     return (
+      <Customizer {...FluentCustomizations}>
       <div className={ styles.taskWebPart }>
         <div className={ styles.container }>
           <div className={ styles.row }>
@@ -20,6 +24,7 @@ export default class TaskWebPart extends React.Component<ITaskWebPartProps, {}> 
           </div>
         </div>
       </div>
+      </Customizer>
     );
   }
 }
